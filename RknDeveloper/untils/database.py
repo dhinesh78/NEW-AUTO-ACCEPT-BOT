3bc69133ba180cfe7dd8536fe8237ff26df4c4f5
Rkn Developer <138338185+RknDeveloper@users.noreply.github.com>
2023-11-13 17:57:58 +0545

    Update database.py

--- RknDeveloper/untils/database.py
+++ RknDeveloper/untils/database.py
@@ -18,12 +18,23 @@ def already_dbg(chat_id):
             return False
         return True
 
+async def add_user(self, b, m):
+        u = m.from_user
+        if not await self.is_user_exist(u.id):
+            user = self.new_user(u.id)
+            await self.col.insert_one(user)            
+            await send_log(b, u)
+                
 def add_user(user_id):
     in_db = already_db(user_id)
     if in_db:
         return
     return users.insert_one({"user_id": str(user_id)}) 
 
+def is_user_exist(user_id):
+    user = users.find_one({"user_id": str(user_id)})
+    return bool(user)
+        
 def remove_user(user_id):
     in_db = already_db(user_id)
     if not in_db:
