ed070d2d438a9c021f6205d18d60cc1d78eed201
dhinesh78 <144138957+dhinesh78@users.noreply.github.com>
2024-04-01 21:50:23 -0700

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,22 +3,22 @@ import os, time
 #from pyrogram.types import BotCommand
 
 class Config:
-    API_ID = int(getenv("API_ID", "0112234"))
-    API_HASH = getenv("API_HASH", "abcdefg")
-    BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:rkndeveloperDEhdhyjjvjjftSEW")
-    UPDATE_CHANNEL = getenv("UPDATE_CHANNEL", "RknDeveloper")
-    UPDATECHANNEL_ID = int(getenv("UPDATECHANNEL_ID", "-1001819787652"))
-    ADMIN = list(map(int, getenv("ADMIN", "6151758586").split()))
+    API_ID = int(getenv("API_ID", "20652787"))
+    API_HASH = getenv("API_HASH", "5dea928561e4d2eb77a371edf8b2eb2a")
+    BOT_TOKEN = getenv("BOT_TOKEN", "6720259127:AAFm3tGJl03N0ByWDOvevGiM_z4lpddeNr4")
+    UPDATE_CHANNEL = getenv("UPDATE_CHANNEL", "DP_BOTZ")
+    UPDATECHANNEL_ID = int(getenv("UPDATECHANNEL_ID", "-1002008853384"))
+    ADMIN = list(map(int, getenv("ADMIN", "1242556540").split()))
     MONGO_URI = getenv("MONGO_URI", "")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002120760645"))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001789244683"))
     
     #web response 
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
     BOT_UPTIME  = time.time()
     PORT = os.environ.get("PORT", "8080")
     
-    RKN_PIC = os.environ.get("RKN_PIC", "https://graph.org/file/e846f9375e9d4f4975ce4.jpg")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME","Rkn_AutoRequestApprovebot")
+    RKN_PIC = os.environ.get("RKN_PIC", "https://te.legra.ph/file/ba16b6f4c78879c5d5527.jpg")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME","Auto_Accept_Dpbot")
  # Subsprice Gif & Auto Request Accept
     SURPRICE = os.environ.get("SURPRICE", "https://telegra.ph/file/a5a2bb456bf3eecdbbb99.mp4 https://telegra.ph/file/03c6e49bea9ce6c908b87.mp4 https://telegra.ph/file/9ebf412f09cd7d2ceaaef.mp4 https://telegra.ph/file/293cc10710e57530404f8.mp4 https://telegra.ph/file/506898de518534ff68ba0.mp4 https://telegra.ph/file/dae0156e5f48573f016da.mp4 https://telegra.ph/file/3e2871e714f435d173b9e.mp4 https://telegra.ph/file/714982b9fedfa3b4d8d2b.mp4 https://telegra.ph/file/876edfcec678b64eac480.mp4 https://telegra.ph/file/6b1ab5aec5fa81cf40005.mp4 https://telegra.ph/file/b4834b434888de522fa49.mp4").split()
 
